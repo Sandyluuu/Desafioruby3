194c1feddc7cdbdd48aa227f18408ea594820634
Sandyluuu <131992993+Sandyluuu@users.noreply.github.com>
2023-06-13 03:20:55 +0000

    empezando IMC ejercicio

--- IMC.rb
+++ IMC.rb
@@ -1,6 +1,19 @@
 #Requerimiento 3, utilizando como base el ejercicio de cálculo de IMC en la guía de ejercicios, agregar en el cálculo de Obesidad los elementos faltantes dado que la obesidad se subdivide en 3 niveles. Deberás agregar a la lógica condicional los cálculos para esos tres niveles.
 
+puts "Ingresa tu peso en Kilogramos: "
+peso = gets.chomp.to_f
+puts "Ingresa tu altura expresada en metros: "
+altura = gets.chomp.to_f
 
-def formula_imc(valor1, valor2)
-imc = valor1 / valor2**2
+def calcular_imc(imc)
+puts("Tu índice de masa corporal es => #{imc}")
+if imc < 18.5
+puts "El índice calculado es Bajo de peso"
+elsif imc <= 18.5 || imc <= 24.9
+puts "El índice calculado es Normal"
+elsif imc <= 25.0 || imc <= 29.9
+puts "El índice calculado es Sobrepeso"
+else
+puts "El índice calculado es Obesidad"
+end
 end
