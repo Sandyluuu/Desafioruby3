788ac7aae4577efc44e351a53bca59fffec17f5a
Sandyluuu <131992993+Sandyluuu@users.noreply.github.com>
2023-06-13 03:13:18 +0000

    agrego loop

---
+++ IMC.rb
@@ -0,0 +1,6 @@
+#Requerimiento 3, utilizando como base el ejercicio de cálculo de IMC en la guía de ejercicios, agregar en el cálculo de Obesidad los elementos faltantes dado que la obesidad se subdivide en 3 niveles. Deberás agregar a la lógica condicional los cálculos para esos tres niveles.
+
+
+def formula_imc(valor1, valor2)
+imc = valor1 / valor2**2
+end
--- main (copy).rb
+++ main (copy).rb
@@ -1,16 +1,19 @@
 #te solicitan un programa que simule el juego de “Adivina el número”. En este programa el computador va a elegir un número aleatorio y el usuario al seleccionar un número debe indicarle si ganó o perdió. La condición para ganar es que el número del computador debe ser igual al del jugador. El rango de número va entre 1 hasta n, tomando en consideración que mientras más amplio sea el rango más duradero será el juego.
 
 def adivina_el_numero
-  puts "Bienvenido a Adivina el Número. Ingresa un número:"
+  puts "Bienvenido a Adivina el Número. Ingresa el rango máximo:"
+  n = gets.chomp.to_i
+  puts "Ingresa un número:"
   numero_usuario = gets.chomp.to_i
-  puts I"ngresa un número:"
   numero_pc = rand(1..n) 
 
+  loop do 
   if numero_usuario == numero_pc
     puts "¡Ganaste! :D El número era #{numero_pc}."
   else
     puts "¡Perdiste! :p El número era #{numero_pc}."
   end
+end
 end
 
 adivina_el_numero
